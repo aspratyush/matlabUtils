e6f0fb015e0c3387fe349e008825d097f325129c
Pratyush Sahay <aspratyush@gmail.com>
2015-01-26 22:50:43 +0530

    Create plotPtCloud.m
    First commit version of plotPtCloud.m

---
+++ plotPtCloud.m
@@ -0,0 +1,67 @@
+%
+% This program is free software; you can redistribute it and/or
+% modify it under the terms of the GNU General Public License
+% version 2 as published by the Free Software Foundation.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+function [] = plotPtCloud(X,varargin)
+
+%PLOTSHAPE  Visual representation of 3D data.
+%
+%   PLOTSHAPE(X) displays the configuration X if X is m-by-k, or the 
+%   superimposed n configurations if X is m-by-k-by-n.
+%
+%   PLOTSHAPE(X,'ProperyName',PropertyValue) alters the plotting style.
+%   The range of available properties is the same as that of MATLAB's 
+%   plot command; see 'help plot' for more info.
+%
+%   PLOTSHAPE(X,'byobs') and PLOTSHAPE(X,'bylandmark') uses different 
+%   marker styles for different observations or landmarks, respectively.
+%
+%	v0.1 original version (Simon Preston)
+%   v0.2 modifications for handling generic point Clouds (Pratyush Sahay)
+
+
+[m,k,n] = size(X);
+noOfPoints = k*n;
+
+holdStatus = ishold;
+
+if m==3,  z = reshape(squeeze(X(3,:,:))',[1 noOfPoints]);  end;
+
+% point Cloud colour
+ptCloudColour = {'b', 'r', 'm', 'k'};
+numCol = numel(ptCloudColour);
+pntSty = {'.', 'o', 'x', 'p', 's', 'd', 'h'};
+numSty = numel(pntSty);
+
+% If no plot style is specified, default to plotting unconnected blue dots.
+if isempty(varargin)
+   varargin{1} = '.';
+end
+
+%visualize the data (2D or 3D) 
+if m == 2
+      x = reshape(squeeze(X(1,:,:))',[1 noOfPoints]);
+      y = reshape(squeeze(X(2,:,:))',[1 noOfPoints]);      
+      plot(x,y,varargin{:});
+elseif m == 3
+      x = reshape(squeeze(X(1,:,:))',[1 noOfPoints]);
+      y = reshape(squeeze(X(2,:,:))',[1 noOfPoints]);
+      z = reshape(squeeze(X(3,:,:))',[1 noOfPoints]);
+      plot3(x,y,z,varargin{:});
+else,  error('Shape data must be two- or three-dimensional');
+end
+
+if ~holdStatus,  hold off;  end;
+
+axis equal;
+axis vis3d;
